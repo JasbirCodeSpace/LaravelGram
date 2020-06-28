25a46478e4a3dc66f4938941c9c36071d0ac5afd
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-06-28 22:24:55 +0530

    Initializing project

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://scontent-del1-1.cdninstagram.com/v/t51.2885-19/s150x150/97566921_2973768799380412_5562195854791540736_n.jpg?_nc_ht=scontent-del1-1.cdninstagram.com&_nc_ohc=aBCmYoNjto4AX-6NMff&oh=2b024509113fb54fce3e3a99912c083d&oe=5F1F4167"
+             class="rounded-circle" alt="LaravelGram">
+        </div>
+        <div class="col-9 pt-3">
+            <div><h1>LaravelGram</h1></div>
+            <div class="d-flex">
+                <div class="pr-3"><strong>51k</strong> followers</div>
+                <div class="pr-3"><strong>261</strong> following</div>
+                <div class="pr-3"><strong>304</strong> posts</div>
+            </div>
+            <div class="pt-4 font-weight-bold">laravelgram.com</div>
+            <div class="pt-1">
+                <p>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</p>
+            </div>
+            <div>
+                <a href="https://www.freecodecamp.org/">www.freecodecamp.org</a>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
