e9218a0e731a0863b5dc79043da838c03521bad8
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-07-02 13:26:04 +0530

    Updated PostsController

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -9,4 +9,12 @@ class PostsController extends Controller
     public function create(){
         return view('posts.create');
     }
+    public function store(){
+        $post = request()->validate([
+            'caption'=>'required',
+            'image'=>['required','image'],
+        ]);
+        \App\Post::create($post);
+        dd(request()->all());
+    }
 }
