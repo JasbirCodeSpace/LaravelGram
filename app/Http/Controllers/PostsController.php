d04ed3aabaf78e36411cca3d9e5dd221d931346b
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-07-03 19:57:11 +0530

    Added protection and image storage of post

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function create(){
         return view('posts.create');
     }
@@ -14,7 +18,14 @@ class PostsController extends Controller
             'caption'=>'required',
             'image'=>['required','image'],
         ]);
-        \App\Post::create($post);
-        dd(request()->all());
+        
+        $imagePath = request('image')->store('uploads','public');
+        auth()->user()->posts()->create([
+            'caption'=>$post['caption'],
+            'image'=>$imagePath,
+
+        ]);
+        
+        return redirect('/profile' . auth()->user()->id);
     }
 }
