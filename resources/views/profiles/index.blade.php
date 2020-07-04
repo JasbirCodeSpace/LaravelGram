6679168410ec1741d829c58a86188d3d0feffe9d
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-07-05 00:39:44 +0530

    Updated index view

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -11,6 +11,7 @@
                 <h1>{{ $user->username }}</h1>
                 <a href="/p/create">Add New Post</a>
             </div>
+            <a href="/profile/{{ $user->id }}/edit">Edit Profile</a>
 
             <div class="d-flex">
                 <div class="pr-3"><strong>{{ $user->posts->count()}}</strong> posts</div>
@@ -30,7 +31,7 @@
         @foreach($user->posts as $post)
         <div class="col-4 pb-4">
             <a href="/p/{{ $post->id }}">
-            <img src="/storage/{{ $post->image }}" alt="" class="w-100">
+                <img src="/storage/{{ $post->image }}" alt="" class="w-100">
             </a>
         </div>
         @endforeach
