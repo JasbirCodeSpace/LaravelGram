c50ba8e9f1c1addd137bff01ab806d4ab540fc8a
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-07-07 23:54:55 +0530

    Updated vue for follow button

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -10,7 +10,7 @@
             <div class="d-flex justify-content-between align-items-baseline">
                 <div class="d-flex align-items-center pb-3">
                     <div class="h4">{{ $user->username }}</div>
-                    <button class="btn btn-primary btn-sm ml-4">Follow</button>
+                    <follow-button user-id="{{$user->id}}"></follow-button>
                 </div>
 
                 @can ('update',$user->profile)
@@ -18,10 +18,7 @@
                 @endcan
             </div>
             @can ('update',$user->profile)
-            <a href="/profile/{{ $user->id }}/edit">Edit Profile</a>
-            @endcan
-
-            <div class="d-flex">
+            <a href="/profile/{{ $user->id }}/edit">Edit Profile</a> @endcan <div class="d-flex">
                 <div class="pr-3"><strong>{{ $user->posts->count()}}</strong> posts</div>
                 <div class="pr-3"><strong>51k</strong> followers</div>
                 <div class="pr-3"><strong>261</strong> following</div>
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,9 @@ Auth::routes();
 Route::get('/profile/{user}/edit', 'ProfilesController@edit')->name('profile.edit');
 Route::patch('/profile/{user}', 'ProfilesController@update')->name('profile.update');
 Route::get('/profile/{user}', 'ProfilesController@index')->name('profile.show');
-Route::get('/p/create', 'PostsController@create');
+
 Route::post('/p', 'PostsController@store');
+Route::get('/p/create', 'PostsController@create');
 Route::get('/p/{post}', 'PostsController@show');
+
+Route::post('follow/{user}', 'FollowsController@store');
