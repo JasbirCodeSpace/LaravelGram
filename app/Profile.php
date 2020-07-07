19f13682e552f92e9502064147b9cef11c1f238f
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-07-07 23:18:17 +0530

    update profile and post page

--- app/Profile.php
+++ app/Profile.php
@@ -7,6 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Profile extends Model
 {
     protected $guarded = [];
+
+    public function profileImage()
+    {
+        $imagePath = ($this->image) ?  $this->image : 'profile/default_user.png';
+        return '/storage/' . $imagePath;
+    }
     public function user()
     {
         return $this->belongsTo(User::class);
